536853ba4ed6827e110e54ce98612777ff855346
segundoc361 <126724727+segundoc361@users.noreply.github.com>
2025-01-29 21:42:32 +0000

    Second commit

---
+++ src/MixMatch.java
@@ -0,0 +1,39 @@
+public class MixMatch {
+
+    // Main
+    public static void main(String[] args2) {
+
+        // int 
+        int Intnum = 10;
+        System.out.println(Intnum + " <- Integer, stores whole numbers from -2,147,483,648 to 2,147,483,647");
+
+        // String
+        String Stringvalue = "String Value";
+        System.out.println(Stringvalue + " <- String, contains a collection of characters surrounded by double quotes");
+
+        // char
+        char charvalue = 'A';
+        System.out.println(charvalue + " <- char, stores a single character/letter or ASCII values");
+
+        // short
+        short shortvalue = 50;
+        System.out.println(shortvalue + " <- short, Stores whole numbers from -32,768 to 32,767");
+
+        // long
+        long longvalue = 80;
+        System.out.println(longvalue + " <- long, Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807");
+
+        // float
+        float floatvalue = 99.99999f;
+        System.out.println(floatvalue + " <- Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits");
+
+        // double
+        double doublevalue = 1.9999999999999;
+        System.out.println(doublevalue + " <- Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits");
+
+        // boolean
+        boolean booleanvalue = true;
+        System.out.println(booleanvalue + " <- Stores true or false values");
+    }
+
+}
